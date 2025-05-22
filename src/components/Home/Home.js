2a724b9a23bd68eabe0c92830f6d38d8275d60c9
Joomun <joomunmuddathir@gmail.com>
2025-05-22 23:50:00 +0400

    Revert "Rework Home"
    This reverts commit 02846c32b295cc1ff6583b7d730f8578a96402c6.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
-import "./Home.css";
 import Type from "./Type";
-import { motion } from "framer-motion";
 
 function Home() {
   return (
@@ -13,69 +11,32 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-          <Row className="align-items-center">
+          <Row>
             <Col md={7} className="home-header">
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-              >
-                <h1 className="heading minimal-heading">
-                  Hello
-                  <motion.span
-                    className="wave"
-                    role="img"
-                    aria-labelledby="wave"
-                    animate={{
-                      rotate: [0, 14, -8, 14, 0],
-                      transition: {
-                        duration: 2.5,
-                        repeat: Infinity,
-                        repeatType: "loop",
-                        ease: "easeInOut",
-                      },
-                    }}
-                    style={{ display: "inline-block", marginLeft: "10px" }}
-                  >
-                    👋🏻
-                  </motion.span>
-                </h1>
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
 
-                <div className="name-container">
-                  <motion.h1
-                    className="heading-name"
-                    initial={{ opacity: 0, x: -20 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.8, delay: 0.2 }}
-                  >
-                    I'm{" "}
-                    <strong className="main-name">Joomun Noor</strong>
-                  </motion.h1>
-                </div>
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Joomun Noor</strong>
+              </h1>
 
-                <motion.div
-                  className="type-container"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.8, delay: 0.4 }}
-                >
-                  <Type />
-                </motion.div>
-              </motion.div>
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
             </Col>
 
-            <Col md={5} className="home-img-container">
-              <motion.div
-                initial={{ opacity: 0, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.8, delay: 0.2 }}
-              >
-                <img
-                  src={homeLogo}
-                  alt="home pic"
-                  className="home-img"
-                />
-              </motion.div>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
             </Col>
           </Row>
         </Container>
