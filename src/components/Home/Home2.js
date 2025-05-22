02846c32b295cc1ff6583b7d730f8578a96402c6
Joomun <joomunmuddathir@gmail.com>
2025-05-22 23:44:54 +0400

    Rework Home

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,103 +1,113 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
+  const fadeIn = {
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.8 }
+  };
+
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
-      <Row>
-        <Col md={8} className="home-about-description">
-          <h1 style={{ fontSize: "2.6em" }}>
-            LET ME <span className="purple"> INTRODUCE </span> MYSELF
-          </h1>
-          <p className="home-about-body">
-            I am a passionate <b className="purple">Packaged App Development Associate</b>, with a strong foundation in{" "}
-            <i>
-              <b className="purple">Linux</b>, <b className="purple">Bash/Shell scripting</b>, and 
-              <b className="purple"> innovative solutions.</b>
-            </i>
-            <br />
-            <br />While my professional career focuses on packaged app development, I also enjoy exploring{" "}
-            <i>
-              <b className="purple">web development</b>, <b className="purple">IoT</b>, and emerging technologies.
-            </i>
-            <br />
-            <br />
-            I have expertise in technologies like{" "}
-            <i>
-              <b className="purple">TypeScript</b>, <b className="purple">Azure services</b>, <b className="purple">AWS (Lambda, S3)</b>, and{" "}
-              <b className="purple">Python development</b>.
-            </i>
-            <br />
-            <br />
-            Additionally, I love experimenting with hardware projects using{" "}
-            <i>
-              <b className="purple">Raspberry Pi</b>.
-            </i>
-            <br />
-            <br />
-            Whenever possible, I contribute to{" "}
-            <b className="purple">open-source projects</b> and collaborate with other tech enthusiasts to create amazing solutions! Great example is my role in 
-            <b className="purple"> IEEE Middlesex Univeristy Student Branch</b>
-          </p>
-        </Col>
-        <Col md={4} className="myAvtar">
-          <Tilt>
-            <img src={myImg} className="img-fluid" alt="avatar" />
-          </Tilt>
-        </Col>
-      </Row>
+        <Row className="align-items-center">
+          <Col md={7} className="home-about-description">
+            <motion.div {...fadeIn}>
+              <h1 className="minimal-heading">
+                About <span className="purple">Me</span>
+              </h1>
+              <p className="home-about-body">
+                I am a passionate <span className="purple">Packaged App Development Associate</span> with expertise in{" "}
+                <span className="purple">Linux</span>, <span className="purple">TypeScript</span>, and{" "}
+                <span className="purple">Cloud Services</span>.
+                <br /><br />
+                My focus areas include:
+                <br />
+                <motion.ul 
+                  className="skill-list"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.3 }}
+                >
+                  <li><span className="purple">▹</span> Web Development</li>
+                  <li><span className="purple">▹</span> Azure & AWS Cloud Services</li>
+                  <li><span className="purple">▹</span> IoT & Raspberry Pi Projects</li>
+                  <li><span className="purple">▹</span> Open Source Contributions</li>
+                </motion.ul>
+                <br />
+                I'm proud to be part of the <span className="purple">IEEE Middlesex University Student Branch</span>, 
+                where I collaborate with fellow tech enthusiasts.
+              </p>
+            </motion.div>
+          </Col>
+          
+          <Col md={5} className="myAvtar">
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.8 }}
+              className="avatar-container"
+            >
+              <img src={myImg} className="about-img" alt="avatar" />
+            </motion.div>
+          </Col>
+        </Row>
 
         <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-            <p>
-              Feel free to <span className="purple">connect </span>with me
-            </p>
-            <ul className="home-about-social-links">
-              <li className="social-icons">
-                <a
+          <Col md={12}>
+            <motion.div 
+              className="home-about-social"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+            >
+              <h2 className="connect-heading">Let's Connect</h2>
+              <div className="social-icons-container">
+                <motion.a
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
                   href="https://github.com/joomun"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="social-icon"
                 >
                   <AiFillGithub />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
+                </motion.a>
+                <motion.a
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
                   href="https://www.linkedin.com/in/joomun-noorani-muddathir-846636228/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="social-icon"
                 >
                   <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
+                </motion.a>
+                <motion.a
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
                   href="https://www.instagram.com/muddathir_joomun/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour home-social-icons"
+                  className="social-icon"
                 >
                   <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
+                </motion.a>
+              </div>
+            </motion.div>
           </Col>
         </Row>
       </Container>
     </Container>
   );
 }
+
 export default Home2;
