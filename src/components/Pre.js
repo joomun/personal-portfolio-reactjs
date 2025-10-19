4d63f6637f01ef5777fc61f997a8a6a4cf6b6777
Joomun <Joomun@Desktop.com>
2025-10-19 20:58:35 +0400

    rework animation

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,30 @@
 import React from "react";
+import "./Animations/Animations.css";
+
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={props.load ? "preloader" : "preloader-none"}>
+      <div className="terminal-loader">
+        <div className="terminal-header">
+          <span className="terminal-dot red"></span>
+          <span className="terminal-dot yellow"></span>
+          <span className="terminal-dot green"></span>
+          <span className="terminal-title">visitor@portfolio:~$</span>
+        </div>
+        <div className="terminal-body">
+          <span className="terminal-prompt">$</span> 
+          <span className="terminal-joke">join in my daily black terminal...</span>
+          <div className="terminal-loading-bar">
+            <span className="loading-bar"></span>
+          </div>
+          <div className="terminal-joke-message">
+            Why do programmers prefer dark mode?<br />
+            <span className="terminal-punchline">Because light attracts bugs! 🐞</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pre;
