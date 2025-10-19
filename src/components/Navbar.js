764e07493cfec2e101b960cc8839f64c09a41f70
Joomun <Joomun@Desktop.com>
2025-10-19 20:49:07 +0400

    Navbar reworlk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,8 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
-import { Link, NavLink } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineFundProjectionScreen,
-  AiOutlineUser,
-  AiOutlinePlayCircle,
-} from "react-icons/ai";
-
-import { CgFileDocument } from "react-icons/cg";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -36,11 +24,11 @@ function NavBar() {
       expanded={expand}
       fixed="top"
       expand="md"
-      className={navColour ? "sticky" : "navbar"}
+      className={navColour ? "sticky minimal-nav" : "navbar minimal-nav"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+        <Navbar.Brand href="/" className="nav-brand">
+          JN
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -49,81 +37,33 @@ function NavBar() {
           }}
         >
           <span></span>
-          <span></span>
-          <span></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/"
-                end
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              <Nav.Link as={NavLink} to="/" end onClick={() => updateExpanded(false)}>
+                Home
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/about"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              <Nav.Link as={NavLink} to="/about" onClick={() => updateExpanded(false)}>
+                About
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} /> Projects
+              <Nav.Link as={NavLink} to="/project" onClick={() => updateExpanded(false)}>
+                Projects
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/mini-game"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} /> Mini-Game
+              <Nav.Link as={NavLink} to="/mini-game" onClick={() => updateExpanded(false)}>
+                Game
               </Nav.Link>
             </Nav.Item>
-
             <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/animations"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlinePlayCircle style={{ marginBottom: "2px" }} /> Animations
+              <Nav.Link as={NavLink} to="/resume" onClick={() => updateExpanded(false)}>
+                Resume
               </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={NavLink}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/joomun"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
